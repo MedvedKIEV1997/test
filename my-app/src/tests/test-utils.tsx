3ec6f7d760536810b2c69737b1d3f1098465274d
MedvedKIEV1997 <burdakigor@gmail.com>
2022-09-08 18:16:49 +0300

    refactoring

--- my-app/src/tests/test-utils.tsx
+++ my-app/src/tests/test-utils.tsx
@@ -4,6 +4,7 @@ import type { RenderOptions } from '@testing-library/react';
 import { PreloadedState } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { AppStore, RootState, setupStore } from '../redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
     preloadedState?: PreloadedState<RootState>;
@@ -20,7 +21,11 @@ export function renderWithProviders(
     }: ExtendedRenderOptions = {}
 ) {
     function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
-        return <Provider store={store}>{children}</Provider>;
+        return (
+            <BrowserRouter>
+                <Provider store={store}>{children}</Provider>
+            </BrowserRouter>
+        );
     }
 
     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
